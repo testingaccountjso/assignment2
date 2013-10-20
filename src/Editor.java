16689ee7e447b3bf8a8fd449c5be9ac76925c2d6
testingaccountjso <b617548@drdrb.com>
2013-10-20 13:58:49 -0600

    editor.java description

---
+++ src/Editor.java
@@ -0,0 +1,23 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jonathan
+ * Date: 10/20/13
+ * Time: 1:05 PM
+ * To change this template use File | Settings | File Templates.
+ *
+ * Your Editor class will have a private StringBuilder instance variable called lineEditor.
+ * You should have at least one constructor with one parameter. This constructor will take in a string.
+ * Inside the constructor, you will instantiate a StringBuilder object with that string.
+ * (Please follow the instructions in this section carefully.)
+ * Besides that, you should also write a setLineEditor that will set
+ * the StringBuilder variable to a new StringBuilder object and
+ * getLineEditor method that will return the content of lineEditor.
+ * You will then write the three methods listed above for each of the tasks that Editor can perform.
+ *
+ */
+public class Editor {
+    private StringBuilder lineEditor;
+
+
+
+}
