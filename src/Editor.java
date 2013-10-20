a9912da2477ee7768d52a3088ae9819f5ca5e150
testingaccountjso <b617548@drdrb.com>
2013-10-20 14:52:14 -0600

    2:53pm

--- src/Editor.java
+++ src/Editor.java
@@ -34,5 +34,16 @@ public class Editor {
 		this.lineEditor = lineEditor;
 	}
 
+	/*Delete string (D). In this method, you will prompt for the index where you want to start deleting a string. You
+	will also prompt for the index of the characters one past the last location to delete. Then use a standard
+	StringBuilder method to help you complete the task and output the updated string. If a user type in an index that
+	is longer than the length of your lineEditor, an exception will occur. Handle this exception by using “try and
+	catch” clauses. Also, output the error message that the exception returns. In addition to that,
+	output another message to explain that the segment of string has not been deleted due to error.  Hint: Read your
+	textbook to find out which type of exception will occur and handle it. Look for clues in Common Programming Error
+	box.*/
+	public StringBuilder deleteString(StringBuilder inString) {
+
+	}
 
 }
